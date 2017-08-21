ba45d05ace29cf79bda65eafc775598e739356f6
qinjingle <qinjingle@itcast.cn>
2017-08-21 20:50:00 +0800

    第四次提交

--- gitTest/src/cn/itcast/git/GitTest.java
+++ gitTest/src/cn/itcast/git/GitTest.java
@@ -3,9 +3,9 @@ package cn.itcast.git;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("我在这里学习呢");
-		System.out.println("asd a");
-		System.out.println("asd a");
-		System.out.println("asd a");
+		System.out.println("java");
+		System.out.println("教学计划");
+		System.out.println("5648665");
 		System.out.println("a565654");
 		System.out.println("asd a");
 		
