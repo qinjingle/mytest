ae9561528a54d54093a93d76418d8b7d372bc7dc
qinjingle <qinjingle@itcast.cn>
2017-08-21 20:16:21 +0800

    是

--- gitTest/src/cn/itcast/git/GitTest.java
+++ gitTest/src/cn/itcast/git/GitTest.java
@@ -3,6 +3,11 @@ package cn.itcast.git;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("我在这里学习呢");
+		System.out.println("asd a");
+		System.out.println("asd a");
+		System.out.println("asd a");
+		System.out.println("asd a");
+		System.out.println("asd a");
 		
 	}
 }
